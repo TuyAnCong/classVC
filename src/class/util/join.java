07da65dea4baf0bc2193d46db61bc5ccfebc6dc6
TuyAnCong <97296405+TuyAnCong@users.noreply.github.com>
2022-03-11 22:03:02 +0700

    Create join.java

---
+++ src/class/util/join.java
@@ -0,0 +1,13 @@
+/**
+* code ::
+* AudioHigh.classVoiceChannel.Bot and Join.Access.Voice
+* @author ClassVC City
+*
+**/
+
+/** Code Of ClassVC and ClassVoice
+* Code Package ClassVoice Is Via VoicelyMusic Fully Function
+* CI ::
+* Tap Action and start a black.yml or ClassVoice: Create a JoinVC
+**/
+{} // wait I start a CI Actions
