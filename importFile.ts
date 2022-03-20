92176a9b8cfbf495d0d4bb6d631b6f63ec061403
TuyAnCong <97296405+TuyAnCong@users.noreply.github.com>
2022-03-20 22:02:04 +0700

    Create importFile.ts

---
+++ importFile.ts
@@ -0,0 +1,6 @@
+import { File, Write } from 'ServerFile'
+const App = require('ServerFile')
+var ServerFile.Folder = '/home'
+File('IMPORT', import_file(JSON.phrse('http://cdn.discordcard.com/image/welcome?user=' + userid), 'image.png', App.ServerUptime.SaveFile(uptime)))
+App.Binary.Server(folder, pingus).join('.')
+App.Res.Server(${ServerFile.Folder})
