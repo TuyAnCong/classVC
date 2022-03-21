f578ae262e2d4847454cd74767f1e723de69ad07
TuyAnCong <97296405+TuyAnCong@users.noreply.github.com>
2022-03-21 20:44:56 +0700

    Create PlayerCreate.java

---
+++ src/class/Music/PlayerCreate.java
@@ -0,0 +1,7 @@
+package PlayerCreate.ActiveCache.MusicBot
+import DiscordBot.MusicJava.Client;
+import cogs.Client.DiscordBot_Client.JadaBot;
+public class Start(voice) {
+client.ready.PlayerCreate('PlayerCreate#ClientNode', 'pronode.tuyancong.xyz', 443, 'nodejsprofiler', true)
+client.ready('PlayerStart', client.JoinChannel(voiceID(clientID)), client.StartPlayMusic(for events.GetUserVarByName('songAdded')))
+}
