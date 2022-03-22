1eb95b30312d36262720a644a08254dd367eb66c
TuyAnCong <97296405+TuyAnCong@users.noreply.github.com>
2022-03-22 07:24:14 +0700

    Create PlayerDelete.java

---
+++ src/class/Music/PlayerDelete.java
@@ -0,0 +1,22 @@
+package PlayerDelete.Musicbot.Set
+import Util.*
+int voice;
+fetch.files(songname)
+int voiceState;
+int Client;
+voice.Destory(voiceID)
+class VoiceState_Leave(){
+Client.Send('disconnected')
+else()
+Client.Send('client not disconnect with no long voice channel class')
+
+}
+VoiceState_Leave()
+int UtilityEndShowMessage;
+/*
+Thank you use import ClassVC and DiscordLyrica 
+
+@import Utility 
+@author ClassVC
+@discordlyrica @classVC 
+*/
